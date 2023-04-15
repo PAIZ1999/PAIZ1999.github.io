0bc1e305af9f537226f01d80d8ee238661646360
PAIZ1999 <121930235+PAIZ1999@users.noreply.github.com>
2023-04-15 18:01:00 +0800

    Update downloadSRT.js

--- downloadSRT.js
+++ downloadSRT.js
@@ -1,66 +1,59 @@
-function padTime(value) {
-  return String(value).padStart(2, '0');
-}
-
-function formatSrtTime(milliseconds) {
-  const date = new Date(milliseconds);
-  const hours = padTime(date.getUTCHours());
-  const minutes = padTime(date.getUTCMinutes());
-  const seconds = padTime(date.getUTCSeconds());
-  const ms = String(date.getUTCMilliseconds()).padStart(3, '0');
-  return `${hours}:${minutes}:${seconds},${ms}`;
-}
-
-function srtSegmentsToBlob(segments) {
-  const srtContent = segments
-    .map((segment, index) => {
-      const start = formatSrtTime(segment.start);
-      const end = formatSrtTime(segment.end);
-      const text = segment.lines.join('\n');
-      return `${index + 1}\n${start} --> ${end}\n${text}\n`;
-    })
-    .join('\n');
-  return new Blob([srtContent], { type: 'text/plain;charset=utf-8' });
-}
-
-async function fetchSrtSegments() {
-  const response = await fetch(document.querySelector('.VideoPlayer video').src);
-  const arrayBuffer = await response.arrayBuffer();
-  const dataView = new DataView(arrayBuffer);
-  const decoder = new TextDecoder('utf-8');
-  const magic = dataView.getUint32(0);
-  if (magic !== 0x4441414d) {
-    throw new Error('Invalid file format');
-  }
-  const segments = [];
-  let offset = 4;
-  while (offset < dataView.byteLength) {
-    const size = dataView.getUint32(offset);
-    const chunk = new Uint8Array(arrayBuffer.slice(offset + 4, offset + 4 + size));
-    const jsonString = decoder.decode(chunk);
-    const segment = JSON.parse(jsonString);
-    segments.push(segment);
-    offset += 4 + size;
-  }
-  return segments;
-}
+const waitFor = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 async function downloadSRT() {
-  try {
-    const segments = await fetchSrtSegments();
-    const blob = srtSegmentsToBlob(segments);
-    const link = document.createElement('a');
-    link.href = URL.createObjectURL(blob);
-    link.download = `${document.querySelector('.VideoPlayer-title').textContent.trim()}.srt`;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  } catch (error) {
-    console.error('Failed to download SRT:', error);
+  // 找到并鼠标悬停在一个菜单的触发图标
+  const menuTrigger = document.querySelector('svg[data-icon="MoreOutlined"]');
+  if (!menuTrigger) {
+    console.error('无法找到菜单触发图标');
+    return;
   }
+  menuTrigger.dispatchEvent(new MouseEvent('mouseover'));
+
+  await waitFor(500);
+
+  // 点击导出妙记选项
+  const exportOption = document.querySelector('.menu-item > span > svg[data-icon="ExportOutlined"]');
+  if (!exportOption) {
+    console.error('无法找到导出妙记选项');
+    return;
+  }
+  exportOption.click();
+
+  await waitFor(500);
+
+  // 点击飞书文档元素以打开下拉菜单
+  const feishuDocOption = document.querySelector('.ud__select__selector__selectItem');
+  if (!feishuDocOption) {
+    console.error('无法找到飞书文档选项');
+    return;
+  }
+  feishuDocOption.click();
+
+  await waitFor(500);
+
+  // 点击 SRT 选项
+  const srtOption = document.querySelector('.ud__select__list__item__content > span > span');
+  if (!srtOption) {
+    console.error('无法找到 SRT 选项');
+    return;
+  }
+  srtOption.click();
+
+  // 取消勾选包含说话人选项
+  const speakerCheckbox = document.querySelector('.ud__checkbox__input[aria-checked="true"]');
+  if (!speakerCheckbox) {
+    console.error('无法找到包含说话人的复选框');
+    return;
+  }
+  speakerCheckbox.click();
+
+  // 点击导出按钮
+  const exportButton = document.querySelector('.ud__button--filled.ud__button--filled-default.ud__button--size-md');
+  if (!exportButton) {
+    console.error('无法找到导出按钮');
+    return;
+  }
+  exportButton.click();
 }
-window.addEventListener('message', (event) => {
-  if (event.data === 'downloadSRT') {
-    downloadSRT();
-  }
-});
+
+downloadSRT();
